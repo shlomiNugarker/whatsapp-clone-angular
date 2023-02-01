62770d6c9d39f88d724a5db039646c752df6fb74
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-01 12:39:54 +0200

    added pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'main-page/:id',
+    component: MainPageComponent,
+  },
+  {
+    path: 'sign-in-page',
+    component: SignInPageComponent,
+  },
+  {
+    path: 'sign-up-page',
+    component: SignUpPageComponent,
+  },
+  {
+    path: 'about-page',
+    component: AboutPageComponent,
+  },
+  {
+    path: '',
+    component: HomePageComponent,
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './cmps/app-root/app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SignInPageComponent } from './pages/sign-in-page/sign-in-page.component';
+import { SignUpPageComponent } from './pages/sign-up-page/sign-up-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent, MainPageComponent, SignInPageComponent, SignUpPageComponent, AboutPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
