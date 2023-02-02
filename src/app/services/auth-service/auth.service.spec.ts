a438c9906e9fb0ceb951fd500064d7c7af75d2b2
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-02 22:15:57 +0200

    auth service

--- src/app/services/auth-service/auth.service.spec.ts
+++ src/app/services/auth-service/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthserviceService } from '../auth-service/authservice.service';
+import { AuthService } from './auth.service';
 
-describe('AuthserviceService', () => {
-  let service: AuthserviceService;
+describe('AuthService', () => {
+  let service: AuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthserviceService);
+    service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
