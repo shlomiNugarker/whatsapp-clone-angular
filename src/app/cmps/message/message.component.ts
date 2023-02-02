53d3604a80e91214c7c0b4f621c2c60a6c692521
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-02 15:21:43 +0200

    message-preview

--- src/app/cmps/message/message.component.ts
+++ src/app/cmps/message/message.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./message.component.scss'],
 })
 export class MessageComponent {
-  messages: number[] = [1, 3, 4, 5, 6, 7, 8, 9.1, 11, 13, 14, 15, 16, 17, 18];
+  messages: number[] = [1, 3, 11, 13, 14, 15, 16, 17, 18];
 }
