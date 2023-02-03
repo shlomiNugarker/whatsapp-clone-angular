1e91fca839a044a0c44a1949186604b67d470291
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-03 12:29:37 +0200

    sign in

--- src/app/pages/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/sign-in-page/sign-in-page.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
-  styleUrls: ['./sign-in-page.component.scss']
+  styleUrls: ['./sign-in-page.component.scss'],
 })
 export class SignInPageComponent {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password).subscribe(() => {
+        console.log('User is logged in');
+        this.router.navigateByUrl('/');
+      });
+    }
+  }
 }
