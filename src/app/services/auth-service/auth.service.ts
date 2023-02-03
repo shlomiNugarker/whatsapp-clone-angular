86c657917d7352cc4efba977a0beb6b5a8ff15d6
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-03 13:44:31 +0200

    AuthService

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3030/api/auth';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    return this.http.post<any>(
+      `${this.apiUrl}/login`,
+      {
+        email,
+        password,
+      },
+      this.httpOptions
+    );
+
+    // this is just the HTTP call,
+    // we still need to handle the reception of the token
+  }
+  signup(email: string, password: string, fullname: string) {
+    return this.http.post<any>(
+      `${this.apiUrl}/signup`,
+      {
+        email,
+        password,
+        fullname,
+      },
+      this.httpOptions
+    );
+  }
+  logout() {
+    return this.http.post<any>(`${this.apiUrl}/logout`, this.httpOptions);
+  }
 }
