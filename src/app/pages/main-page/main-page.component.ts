b57a0e04f3e2eb31ed1172c481afa3b476f35004
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-03 16:09:14 +0200

    loadChats

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Chat } from 'src/app/models/chat';
+import { ChatService } from 'src/app/services/chat-service/chat.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  chats: Chat[] | null = null;
 
+  constructor(private chatService: ChatService) {}
+
+  ngOnInit(): void {
+    this.loadChats();
+  }
+
+  loadChats() {
+    this.chatService
+      .getChats()
+      .subscribe((chats: Chat[]) => (this.chats = chats));
+    console.log(this.chats);
+  }
 }
