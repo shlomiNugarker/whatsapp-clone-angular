f2aabbd2237f9b1a30b550c0d29d3dea4d810084
Shlomi Nugarker <shlomin1231@gmail.com>
2023-02-08 11:19:11 +0200

    the profile cmp shows the logged user

--- src/app/cmps/profile/profile.component.ts
+++ src/app/cmps/profile/profile.component.ts
@@ -1,15 +1,32 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   @Output('selectModal') onSelectModal = new EventEmitter<string>();
+  constructor(private authService: AuthService) {}
 
   isEditName = false;
   isEditAbout = false;
+  currentUser: User | null = null;
+  userSubScription: Subscription | undefined;
+  fullnameToEdit: string | undefined;
+  aboutToEdit: string | undefined;
+
+  ngOnInit(): void {
+    this.userSubScription = this.authService.currentUser$.subscribe((user) => {
+      this.currentUser = user;
+    });
+
+    this.fullnameToEdit = this.currentUser?.fullname || '';
+    this.aboutToEdit = this.currentUser?.about || '';
+  }
 
   editName() {
     this.isEditName = true;
